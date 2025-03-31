2fdc8296026cae72047f326c1057b482591da770
Raphael Aljona <raphael.aljona@gmail.com>
2025-03-31 08:37:00 -0300

    Jogo do reflexo pronto

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,45 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const int led = 18;
-const int butao = 23;
+const int button = 23;
 
-void setup(){
-    pinMode (led, OUTPUT);
-    pinMode (butao, INPUT_PULLUP);
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+void setup() {
+    pinMode(led, OUTPUT);
+    pinMode(button, INPUT_PULLUP);
 
     lcd.init();
     lcd.backlight();
-    Serial.begin(9600);
-    lcd.setCursor(0, 0);
-    lcd.print("Aperte o botao");
-    lcd.print("para começar");
+    lcd.print("Jogo de Reflexo");
+    delay(2000);
+    lcd.clear();
 }
 
-void loop(){
-    bool estadoAtual = digitalRead(butao);
-    static bool estadoAnterior = 0;
-    unsigned long tempo = millis();
-    unsigned long tempoEspera = random(1000,5000);
-    int count;
-    Serial.println(estadoAtual);
-    Serial.println(estadoAnterior);
+void loop() {
+    lcd.setCursor(0, 0);
+    lcd.print("Prepare-se...");
+    delay(random(1000, 6000));
 
-    if (estadoAtual && !estadoAnterior){
-        count ++;
-        Serial.println(count);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pressione!");
+    digitalWrite(led, HIGH);
+
+    unsigned long startTime = millis();
+    while (digitalRead(button) == HIGH) {
     }
+    unsigned long reactionTime = millis() - startTime;
 
-    if (count >= 1){
-        tempo = tempoEspera;
-        Serial.println("foi");
-        if(tempoEspera == tempoEspera){
-            digitalWrite(led, HIGH);
-            tempo = 0;
-            Serial.print("tempo de espera = tempo");
-        }
-    }
-
-    estadoAnterior = estadoAtual;
-
-    delay(1000);
+    digitalWrite(led, LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tempo: ");
+    lcd.setCursor(7, 0);
+    lcd.print(reactionTime);
+    lcd.print(" ms");
+    delay(5000);
+    lcd.clear();
 }
